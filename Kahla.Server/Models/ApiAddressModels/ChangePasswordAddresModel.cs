29f62217413a82b9792c0f131c5e5698bf6ea860
Anduin2017 <Anduin@aiursoft.com>
2018-10-24 01:04:14 +0800

    limit password length to 6-32.

--- Kahla.Server/Models/ApiAddressModels/ChangePasswordAddresModel.cs
+++ Kahla.Server/Models/ApiAddressModels/ChangePasswordAddresModel.cs
@@ -9,12 +9,18 @@ namespace Kahla.Server.Models.ApiAddressModels
     public class ChangePasswordAddresModel
     {
         [Required]
+        [MinLength(6)]
+        [MaxLength(32)]
         public string OldPassword { get; set; }
 
         [Required]
+        [MinLength(6)]
+        [MaxLength(32)]
         public string NewPassword { get; set; }
 
         [Required]
+        [MinLength(6)]
+        [MaxLength(32)]
         [Compare(nameof(NewPassword), ErrorMessage = "The new password and confirmation password do not match.")]
         public string RepeatPassword { get; set; }
     }
