85718502c3d546474fc755ee2c1593f53c529e2d
Anduin2017 <Anduin@aiursoft.com>
2018-12-21 11:17:06 +0800

    Fix an issue that report API returns error.

--- Kahla.Server/Migrations/20181221031649_AddColumForReport.Designer.cs
+++ Kahla.Server/Migrations/20181221031649_AddColumForReport.Designer.cs
@@ -4,14 +4,16 @@ using Kahla.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kahla.Server.Migrations
 {
     [DbContext(typeof(KahlaDbContext))]
-    partial class KahlaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181221031649_AddColumForReport")]
+    partial class AddColumForReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Kahla.Server/Migrations/20181221031649_AddColumForReport.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kahla.Server.Migrations
+{
+    public partial class AddColumForReport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Reports",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Reports");
+        }
+    }
+}
