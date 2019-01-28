fb8982475bdc61086c8c0b0fc50ea26c1e5f62c3
Anduin2017 <anduin@aiursoft.com>
2019-01-28 22:05:55 +0300

    Reverse json ignore.

--- Kahla.Server/Models/KahlaUser.cs
+++ Kahla.Server/Models/KahlaUser.cs
@@ -11,38 +11,30 @@ namespace Kahla.Server.Models
 {
     public class KahlaUser : AiurUserBase
     {
-        [JsonIgnore]
         [InverseProperty(nameof(PrivateConversation.RequestUser))]
         public IEnumerable<PrivateConversation> Friends { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(PrivateConversation.TargetUser))]
         public IEnumerable<PrivateConversation> OfFriends { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(UserGroupRelation.User))]
         public IEnumerable<UserGroupRelation> GroupsJoined { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(GroupConversation.Owner))]
         public IEnumerable<GroupConversation> GroupsCreated { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(Message.Sender))]
         public IEnumerable<Message> MessagesSent { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(Report.Trigger))]
         public IEnumerable<Report> Reported { get; set; }
 
-        [JsonIgnore]
         [InverseProperty(nameof(Report.Target))]
         public IEnumerable<Report> ByReported { get; set; }
 
-        [JsonIgnore]
         public int CurrentChannel { get; set; } = -1;
-        [JsonIgnore]
         public string ConnectKey { get; set; }
+        [JsonProperty]
         public bool MakeEmailPublic { get; set; } = true;
         public override string Email { get; set; }
         public bool ShouldSerializeEmail() => MakeEmailPublic;
