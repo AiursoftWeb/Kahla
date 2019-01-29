ff348c9e612a9003bf6e91f2291e6cb8b844de06
Anduin2017 <anduin@aiursoft.com>
2019-01-30 02:49:15 +0800

    Always return my email when calling me API

--- Kahla.Server/Controllers/AuthController.cs
+++ Kahla.Server/Controllers/AuthController.cs
@@ -143,6 +143,7 @@ namespace Kahla.Server.Controllers
         {
             var user = await GetKahlaUser();
             user = await _authService.OnlyUpdate(user);
+            user.MakeEmailPublic = true;
             return this.AiurJson(new AiurValue<KahlaUser>(user)
             {
                 Code = ErrorType.Success,
