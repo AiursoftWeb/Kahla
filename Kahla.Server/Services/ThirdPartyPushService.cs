805f2c47b4ce326d0d4bfab3c0bfd6668eba5802
Anduin2017 <anduin@aiursoft.com>
2019-02-24 00:38:10 +0800

    Handle push event error and log it.

--- Kahla.Server/Services/ThirdPartyPushService.cs
+++ Kahla.Server/Services/ThirdPartyPushService.cs
@@ -1,6 +1,7 @@
 ﻿using Kahla.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,24 +15,27 @@ namespace Kahla.Server.Services
         private readonly KahlaDbContext _dbContext;
         private readonly IConfiguration _configuration;
         private readonly WebPushClient _webPushClient;
+        private readonly ILogger _logger;
 
         public ThirdPartyPushService(
             KahlaDbContext dbContext,
             IConfiguration configuration,
-            WebPushClient webPushClient)
+            WebPushClient webPushClient,
+            ILogger<ThirdPartyPushService> logger)
         {
             _dbContext = dbContext;
             _configuration = configuration;
             _webPushClient = webPushClient;
+            _logger = logger;
         }
 
         public async Task PushAsync(string recieverId, string triggerEmail, string payload)
         {
             var user = _dbContext
                 .Users
-                .Include(t=>t.HisDevices)
-                .SingleOrDefault(t=>t.Id == recieverId);
-            if(user == null)
+                .Include(t => t.HisDevices)
+                .SingleOrDefault(t => t.Id == recieverId);
+            if (user == null)
             {
                 throw new ArgumentNullException($"User with id :{recieverId} is null!");
             }
@@ -44,7 +48,15 @@ namespace Kahla.Server.Services
             {
                 var pushSubscription = new PushSubscription(device.PushEndpoint, device.PushP256DH, device.PushAuth);
                 var vapidDetails = new VapidDetails("mailto:" + triggerEmail, vapidPublicKey, vapidPrivateKey);
-                await _webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+                _logger.LogInformation($"Trying to call WebPush API to push a new event to {user.Id}, Event content is '{payload}', Device ID is {device.Id}");
+                try
+                {
+                    await _webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogCritical(e, "An error occoured while calling WebPush API: " + e.Message);
+                }
             }
         }
     }
