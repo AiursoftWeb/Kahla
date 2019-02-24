1eabe37165cce765649eec1cf6b5ce03094a4d40
Anduin2017 <anduin@aiursoft.com>
2019-02-24 22:51:43 +0800

    hide some secret values.

--- Kahla.Server/Models/Device.cs
+++ Kahla.Server/Models/Device.cs
@@ -13,8 +13,11 @@ namespace Kahla.Server.Models
         [ForeignKey(nameof(UserID))]
         public KahlaUser KahlaUser { get; set; }
 
+        [JsonIgnore]
         public string PushEndpoint { get; set; }
+        [JsonIgnore]
         public string PushP256DH { get; set; }
+        [JsonIgnore]
         public string PushAuth { get; set; }
         public DateTime AddTime { get; set; } = DateTime.UtcNow;
     }
