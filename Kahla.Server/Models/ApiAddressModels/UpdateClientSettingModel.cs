adaf25725614dad0a4452a139248dbfcb443b208
hv0905 <hv0905@outlook.com>
2019-03-22 21:48:28 +0800

    Add UpdateClientSetting to save setting

---
+++ Kahla.Server/Models/ApiAddressModels/UpdateClientSettingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Kahla.Server.Models.ApiAddressModels
+{
+    public class UpdateClientSettingAddressModel
+    {
+        [Required]
+        [Range(0,1)]
+        public int ThemeId { get; set; }
+    }
+}
