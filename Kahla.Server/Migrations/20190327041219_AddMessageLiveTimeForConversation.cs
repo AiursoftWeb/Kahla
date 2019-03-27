27bb324602bda046c692f5c758cd9f0c7f485104
Anduin2017 <anduin@aiursoft.com>
2019-03-27 13:12:59 +0900

    Add conversation live time.

---
+++ Kahla.Server/Migrations/20190327041219_AddMessageLiveTimeForConversation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kahla.Server.Migrations
+{
+    public partial class AddMessageLiveTimeForConversation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxLiveSeconds",
+                table: "Conversations",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxLiveSeconds",
+                table: "Conversations");
+        }
+    }
+}
