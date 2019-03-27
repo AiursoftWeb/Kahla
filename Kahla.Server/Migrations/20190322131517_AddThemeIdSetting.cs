4996a70724d9db1e92a3e59d618ca4df4dfd4b41
Anduin2017 <anduin@aiursoft.com>
2019-03-27 13:49:28 +0900

    Max time default to int.max.

--- Kahla.Server/Migrations/20190322131517_AddThemeIdSetting.cs
+++ Kahla.Server/Migrations/20190322131517_AddThemeIdSetting.cs
@@ -10,7 +10,7 @@ namespace Kahla.Server.Migrations
                 name: "ThemeId",
                 table: "AspNetUsers",
                 nullable: false,
-                defaultValue: 0);
+                defaultValue: int.MaxValue);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
