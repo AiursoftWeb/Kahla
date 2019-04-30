bacee4374e6781dc0145deeccaa81a4ec041dec9
Anduin2017 <anduin@aiursoft.com>
2019-04-30 17:22:41 +0800

    Fix possible message crash.

--- Kahla.Server/Models/At.cs
+++ Kahla.Server/Models/At.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -8,13 +9,16 @@ namespace Kahla.Server.Models
 {
     public class At
     {
+        [JsonIgnore]
         public int Id { get; set; }
-
         public string TargetUserId { get; set; }
+        [JsonIgnore]
         [ForeignKey(nameof(TargetUserId))]
         public KahlaUser TargetUser { get; set; }
 
+        [JsonIgnore]
         public int MessageId { get; set; }
+        [JsonIgnore]
         [ForeignKey(nameof(MessageId))]
         public Message Message { get; set; }
     }
