338857e499de577a53c9830aa53bc965b7567ff2
Anduin2017 <anduin@aiursoft.com>
2019-05-14 18:31:33 +0800

    Support take and skip for my friends API.

---
+++ Kahla.Server/Models/ApiAddressModels/MyFriendsAddressModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kahla.Server.Models.ApiAddressModels
+{
+    public class MyFriendsAddressModel
+    {
+        [Required]
+        public bool OrderByName { get; set; } = false;
+
+        [Range(1, 100)]
+        public int Take { get; set; } = 15;
+
+        [Range(1, int.MaxValue)]
+        public int Skip { get; set; } = 0;
+    }
+}
