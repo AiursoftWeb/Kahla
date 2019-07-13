721d59e453b292f39496c9c2ead11d25bfd4e1f8
Anduin Xue <anduin@aiursoft.com>
2019-07-13 22:01:26 +0800

    Remove a useless action.

--- Kahla.Server/Controllers/HomeController.cs
+++ Kahla.Server/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Aiursoft.Pylon.Models;
 using Aiursoft.Pylon.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.IO;
 
 namespace Kahla.Server.Controllers
 {
@@ -16,12 +15,6 @@ namespace Kahla.Server.Controllers
         public HomeController(ServiceLocation serviceLocation)
         {
             _serviceLocation = serviceLocation;
-        }
-
-        [AiurNoCache]
-        public IActionResult Links()
-        {
-            return PhysicalFile(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar +  "assetlink.json", "application/json");
         }
 
         [APIProduces(typeof(AiurValue<DateTime>))]
