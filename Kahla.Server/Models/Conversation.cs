d343b06aa10671c25eeabf99d85e89e91b1f8a9f
Anduin2017 <anduin@aiursoft.com>
2019-08-16 16:57:59 +0800

    Better order.

--- Kahla.Server/Models/Conversation.cs
+++ Kahla.Server/Models/Conversation.cs
@@ -27,13 +27,13 @@ namespace Kahla.Server.Models
         [NotMapped]
         public string DisplayImagePath { get; set; }
 
-        public abstract Conversation Build(string userId);
         public abstract string GetDisplayName(string userId);
         public abstract string GetDisplayImagePath(string userId);
         public abstract int GetUnReadAmount(string userId);
         public abstract bool IWasAted(string userId);
-        public abstract Task ForEachUserAsync(Func<KahlaUser, UserGroupRelation, Task> function, UserManager<KahlaUser> userManager);
+        public abstract Conversation Build(string userId);
         public abstract Message GetLatestMessage();
+        public abstract Task ForEachUserAsync(Func<KahlaUser, UserGroupRelation, Task> function, UserManager<KahlaUser> userManager);
         public abstract Task<DateTime> SetLastRead(KahlaDbContext dbContext, string userId);
         public abstract Task<bool> Joined(KahlaDbContext dbContext, string userId);
     }
