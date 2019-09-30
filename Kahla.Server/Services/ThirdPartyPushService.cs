c5ed3c8acbbea9e0716880491edf7c52d898d2ac
Anduin Xue <anduin@aiursoft.com>
2019-09-30 19:55:58 +0800

    Different dbcontext to delete device.

--- Kahla.Server/Services/ThirdPartyPushService.cs
+++ Kahla.Server/Services/ThirdPartyPushService.cs
@@ -2,6 +2,7 @@
 using Microsoft.ApplicationInsights;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,62 +14,66 @@ namespace Kahla.Server.Services
 {
     public class ThirdPartyPushService
     {
-        private readonly KahlaDbContext _dbContext;
         private readonly IConfiguration _configuration;
         private readonly WebPushClient _webPushClient;
         private readonly ILogger _logger;
         private readonly TelemetryClient _telemetry;
+        private readonly IServiceScopeFactory _scopeFactory;
 
         public ThirdPartyPushService(
-            KahlaDbContext dbContext,
             IConfiguration configuration,
             WebPushClient webPushClient,
             ILogger<ThirdPartyPushService> logger,
-            TelemetryClient telemetry)
+            TelemetryClient telemetry,
+            IServiceScopeFactory scopeFactory)
         {
-            _dbContext = dbContext;
             _configuration = configuration;
             _webPushClient = webPushClient;
             _logger = logger;
             _telemetry = telemetry;
+            _scopeFactory = scopeFactory;
         }
 
         public async Task PushAsync(string receiverId, string triggerEmail, string payload)
         {
-            var devices = await _dbContext.Devices.Where(t => t.UserId == receiverId).ToListAsync();
-            string vapidPublicKey = _configuration.GetSection("VapidKeys")["PublicKey"];
-            string vapidPrivateKey = _configuration.GetSection("VapidKeys")["PrivateKey"];
-            // Push to all devices.
-
-            var pushTasks = new List<Task>();
-            foreach (var device in devices)
+            using (var scope = _scopeFactory.CreateScope())
             {
-                async Task PushToDevice()
+                var dbContext = scope.ServiceProvider.GetRequiredService<KahlaDbContext>();
+                var devices = await dbContext.Devices.Where(t => t.UserId == receiverId).ToListAsync();
+                string vapidPublicKey = _configuration.GetSection("VapidKeys")["PublicKey"];
+                string vapidPrivateKey = _configuration.GetSection("VapidKeys")["PrivateKey"];
+                // Push to all devices.
+
+                var pushTasks = new List<Task>();
+                foreach (var device in devices)
                 {
-                    try
+                    async Task PushToDevice()
                     {
-                        var pushSubscription = new PushSubscription(device.PushEndpoint, device.PushP256DH, device.PushAuth);
-                        var vapidDetails = new VapidDetails("mailto:" + triggerEmail, vapidPublicKey, vapidPrivateKey);
-                        await _webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+                        try
+                        {
+                            var pushSubscription = new PushSubscription(device.PushEndpoint, device.PushP256DH, device.PushAuth);
+                            var vapidDetails = new VapidDetails("mailto:" + triggerEmail, vapidPublicKey, vapidPrivateKey);
+                            await _webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+                        }
+                        catch (WebPushException e)
+                        {
+                            dbContext.Devices.Remove(device);
+                            await dbContext.SaveChangesAsync();
+                            _logger.LogCritical(e, "A WebPush error occured while calling WebPush API: " + e.Message);
+                            _telemetry.TrackException(e);
+                        }
+                        catch (Exception e)
+                        {
+                            _telemetry.TrackException(e);
+                            _logger.LogCritical(e, "An error occured while calling WebPush API: " + e.Message);
+                        }
                     }
-                    catch (WebPushException e)
-                    {
-                        _dbContext.Devices.Remove(device);
-                        await _dbContext.SaveChangesAsync();
-                        _logger.LogCritical(e, "A WebPush error occured while calling WebPush API: " + e.Message);
-                        _telemetry.TrackException(e);
-                    }
-                    catch (Exception e)
-                    {
-                        _telemetry.TrackException(e);
-                        _logger.LogCritical(e, "An error occured while calling WebPush API: " + e.Message);
-                    }
+                    pushTasks.Add(PushToDevice());
                 }
-                pushTasks.Add(PushToDevice());
+                await Task.WhenAny(
+                    Task.WhenAll(pushTasks),
+                    Task.Delay(2000));
             }
-            await Task.WhenAny(
-                Task.WhenAll(pushTasks),
-                Task.Delay(2000));
         }
     }
 }
