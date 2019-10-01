7b36c7e7cc7f952b615214758bc976d9b6be55ea
Anduin Xue <anduin@aiursoft.com>
2019-10-01 23:03:20 +0800

    Fix an issue that personal conversation may 500.

--- Kahla.Server/Controllers/ConversationController.cs
+++ Kahla.Server/Controllers/ConversationController.cs
@@ -157,7 +157,7 @@ namespace Kahla.Server.Controllers
                                 devices: eachUser.HisDevices,
                                 conversation: target,
                                 message: message,
-                                pushAlert: eachUser.Id != user.Id && (mentioned || !relation.Muted),
+                                pushAlert: eachUser.Id != user.Id && (mentioned || !(relation?.Muted ?? false)),
                                 mentioned: mentioned
                                 );
             });
