0b5016480838315e8a4bb8cff447ad1d1edab3b9
Anduin2017 <anduin@aiursoft.com>
2019-11-01 15:43:36 +0800

    Message Id required.

--- Kahla.Server/Models/ApiAddressModels/SendMessageAddressModel.cs
+++ Kahla.Server/Models/ApiAddressModels/SendMessageAddressModel.cs
@@ -19,6 +19,7 @@ namespace Kahla.Server.Models.ApiAddressModels
         /// Guid. For example: 8fe1dd34-7430-4650-8b0a-8587d39dd412
         /// </summary>
         [IsGuid]
+        [Required]
         public string MessageId { get; set; }
     }
 }
