d3c28a72253fd42387bf666bd5b674ba4f321576
Anduin2017 <anduin@aiursoft.com>
2019-12-12 10:16:15 +0800

    Save cookies to file.

--- Kahla.SDK/Services/SingletonHTTP.cs
+++ Kahla.SDK/Services/SingletonHTTP.cs
@@ -1,9 +1,11 @@
 ﻿using Aiursoft.Pylon.Interfaces;
 using Aiursoft.Pylon.Models;
+using Aiursoft.Pylon.Services;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text.RegularExpressions;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 
 namespace Kahla.SDK.Services
@@ -11,15 +13,15 @@ namespace Kahla.SDK.Services
     public class SingletonHTTP : ISingletonDependency
     {
         private readonly HttpClient _client;
-        private readonly Regex _regex;
+        private readonly CookieContainer _cookieContainer;
 
         public SingletonHTTP()
         {
-            _regex = new Regex("^https://", RegexOptions.Compiled);
+            _cookieContainer = Load();
             _client = new HttpClient(new HttpClientHandler()
             {
                 AllowAutoRedirect = false,
-                CookieContainer = new CookieContainer(),
+                CookieContainer = _cookieContainer,
                 UseCookies = true
             });
         }
@@ -34,6 +36,7 @@ namespace Kahla.SDK.Services
             var response = await _client.SendAsync(request);
             if (response.StatusCode == HttpStatusCode.Redirect)
             {
+                Save(_cookieContainer);
                 return response.Headers.Location.OriginalString;
             }
             else
@@ -54,6 +57,7 @@ namespace Kahla.SDK.Services
             var response = await _client.SendAsync(request);
             if (response.IsSuccessStatusCode)
             {
+                Save(_cookieContainer);
                 return await response.Content.ReadAsStringAsync();
             }
             else
@@ -72,11 +76,40 @@ namespace Kahla.SDK.Services
             var response = await _client.SendAsync(request);
             if (response.IsSuccessStatusCode)
             {
+                Save(_cookieContainer);
                 return await response.Content.ReadAsStringAsync();
             }
             else
             {
                 throw new WebException($"The remote server returned unexpcted status code: {response.StatusCode} - {response.ReasonPhrase}.");
+            }
+        }
+
+        public void Save(CookieContainer cookieContainer)
+        {
+            using MemoryStream stream = new MemoryStream();
+            new BinaryFormatter().Serialize(stream, cookieContainer);
+            var bytes = new byte[stream.Length];
+            stream.Position = 0;
+            stream.Read(bytes, 0, bytes.Length);
+            var base64 = bytes.BytesToBase64();
+            File.WriteAllText("cookies.dat", base64);
+        }
+
+        public CookieContainer Load()
+        {
+            try
+            {
+                var base64 = File.ReadAllText("cookies.dat");
+                var bytes = base64.Base64ToBytes();
+                using (MemoryStream stream = new MemoryStream(bytes))
+                {
+                    return (CookieContainer)new BinaryFormatter().Deserialize(stream);
+                }
+            }
+            catch
+            {
+                return new CookieContainer();
             }
         }
     }
