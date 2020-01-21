d6d2866a52725dd8c3e5a547f9384c922ee04761
Anduin2017 <anduin@aiursoft.com>
2020-01-21 21:56:03 +0800

    Add invisiable options.

--- Kahla.Server/Migrations/20200121135545_Invisi.Designer.cs
+++ Kahla.Server/Migrations/20200121135545_Invisi.Designer.cs
@@ -4,14 +4,16 @@ using Kahla.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kahla.Server.Migrations
 {
     [DbContext(typeof(KahlaDbContext))]
-    partial class KahlaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200121135545_Invisi")]
+    partial class Invisi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
