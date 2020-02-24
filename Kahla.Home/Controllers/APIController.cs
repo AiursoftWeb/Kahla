e6d26bcf7909406526638874427d10121ee7442d
Anduin2017 <anduin@aiursoft.com>
2020-02-24 13:14:11 +0800

    Reverse proxy servers list.

--- Kahla.Home/Controllers/APIController.cs
+++ Kahla.Home/Controllers/APIController.cs
@@ -1,5 +1,11 @@
 ﻿using Aiursoft.Handler.Attributes;
+using Aiursoft.XelNaga.Models;
+using Aiursoft.XelNaga.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Kahla.Home.Controllers
 {
@@ -8,6 +14,24 @@ namespace Kahla.Home.Controllers
     [APIModelStateChecker]
     public class APIController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly HTTPService _httpService;
 
+        public APIController(
+            IConfiguration configuration,
+            HTTPService httpService)
+        {
+            _configuration = configuration;
+            _httpService = httpService;
+        }
+
+        [Route("servers")]
+        public async Task<IActionResult> KahlaServerList()
+        {
+            var serversFileAddress = _configuration["KahlaServerList"];
+            var serversJson = await _httpService.Get(new AiurUrl(serversFileAddress), false);
+            var servers = JsonConvert.DeserializeObject<List<string>>(serversJson);
+            return Json(servers);
+        }
     }
 }
