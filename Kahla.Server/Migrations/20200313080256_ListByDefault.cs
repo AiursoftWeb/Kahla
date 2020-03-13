5abf335ed69da4fffb16347bdc0fadf6864e5d67
Anduin2017 <anduin@aiursoft.com>
2020-03-13 16:03:24 +0800

    List in search result by default.

---
+++ Kahla.Server/Migrations/20200313080256_ListByDefault.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kahla.Server.Migrations
+{
+    public partial class ListByDefault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update AspNetUsers set ListInSearchResult = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
