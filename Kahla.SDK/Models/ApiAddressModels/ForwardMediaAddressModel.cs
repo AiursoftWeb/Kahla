5a79a3424e9eb9716a4db57248b7c348d693d534
Anduin Xue <anduin@aiursoft.com>
2020-05-01 18:32:19 +0800

    Fix some required inputs.

--- Kahla.SDK/Models/ApiAddressModels/ForwardMediaAddressModel.cs
+++ Kahla.SDK/Models/ApiAddressModels/ForwardMediaAddressModel.cs
@@ -1,10 +1,16 @@
-﻿namespace Kahla.SDK.Models.ApiAddressModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kahla.SDK.Models.ApiAddressModels
 {
     public class ForwardMediaAddressModel
     {
+        [Required]
         public int SourceConversationId { get; set; }
+        [Required]
         public string FileUploadDate { get; set; }
+        [Required]
         public string FileName { get; set; }
+        [Required]
         public int TargetConversationId { get; set; }
     }
 }
