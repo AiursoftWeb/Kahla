33f202139b1c2eb8862763975f664a93a8a1c9a8
Anduin Xue <anduin@aiursoft.com>
2020-05-19 11:57:27 +0800

    Upgrade version number.

--- Kahla.Server/Startup.cs
+++ Kahla.Server/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Aiursoft.Archon.SDK.Services;
 using Aiursoft.Pylon;
 using Aiursoft.SDK;
-using EFCoreSecondLevelCacheInterceptor;
 using Kahla.SDK.Models;
 using Kahla.Server.Data;
 using Kahla.Server.Middlewares;
@@ -9,11 +8,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Collections.Generic;
 using WebPush;
 
@@ -22,7 +19,6 @@ namespace Kahla.Server
     public class Startup
     {
         private IConfiguration _configuration { get; }
-        private const string _cacheProviderName = "redis";
 
         public Startup(IConfiguration configuration)
         {
@@ -33,30 +29,7 @@ namespace Kahla.Server
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var useRedis = _configuration["easycaching:enabled"] == true.ToString();
-
-            services.AddDbContextPool<KahlaDbContext>((serviceProvider, optionsBuilder) =>
-                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DatabaseConnection"))
-                    .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>()));
-            services.AddEFSecondLevelCache(options =>
-            {
-                if (useRedis)
-                {
-                    options.UseEasyCachingCoreProvider(_cacheProviderName).DisableLogging(true);
-                }
-                else
-                {
-                    options.UseMemoryCacheProvider().DisableLogging(true);
-                }
-                options.CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(30));
-            });
-            if (useRedis)
-            {
-                services.AddEasyCaching(option =>
-                {
-                    option.UseRedis(_configuration, _cacheProviderName);
-                });
-            }
+            services.AddDbContextWithCache<KahlaDbContext>(_configuration.GetConnectionString("DatabaseConnection"));
 
             services.AddIdentity<KahlaUser, IdentityRole>()
                 .AddEntityFrameworkStores<KahlaDbContext>()
