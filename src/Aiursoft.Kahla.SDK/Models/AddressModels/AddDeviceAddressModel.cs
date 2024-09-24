beb7a8140a899761310ae07f39f9b48f9c9f771a
Anduin Xue <anduin@aiursoft.com>
2024-09-24 11:40:27 +0000

    Add migration for Device entity and update annotations
    Introduced a migration reflecting the structure of the Device entity. Updated string length constraints for PushEndpoint, PushP256Dh, and PushAuth properties in the Device class and AddDeviceAddressModel. This ensures better alignment with the database schema and improves data validation.

--- src/Aiursoft.Kahla.SDK/Models/AddressModels/AddDeviceAddressModel.cs
+++ src/Aiursoft.Kahla.SDK/Models/AddressModels/AddDeviceAddressModel.cs
@@ -8,13 +8,13 @@ namespace Aiursoft.Kahla.SDK.Models.AddressModels
         [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
         public string? Name { get; set; }
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [StringLength(400, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
         public string? PushEndpoint { get; set; }
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [StringLength(400, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
         public string? PushP256Dh { get; set; }
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [StringLength(150, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
         public string? PushAuth { get; set; }
     }
 }
--- src/Aiursoft.Kahla.SDK/Models/Device.cs
+++ src/Aiursoft.Kahla.SDK/Models/Device.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
@@ -5,8 +6,13 @@ namespace Aiursoft.Kahla.SDK.Models
 {
     public class Device
     {
+        [Key]
         public long Id { get; init; }
+        [Required]
+        [StringLength(30)]
         public required string Name { get; set; }
+        [Required]
+        [StringLength(40)]
         public required string IpAddress { get; init; }
         
         [JsonIgnore]
@@ -16,10 +22,13 @@ namespace Aiursoft.Kahla.SDK.Models
         public KahlaUser? KahlaUser { get; init; }
 
         [JsonIgnore]
+        [StringLength(400)]
         public required string PushEndpoint { get; set; }
         [JsonIgnore]
+        [StringLength(400)]
         public required string PushP256Dh { get; set; }
         [JsonIgnore]
+        [StringLength(150)]
         public required string PushAuth { get; set; }
         
         public DateTime AddTime { get; init; } = DateTime.UtcNow;
