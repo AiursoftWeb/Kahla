0b3ce683ba3619f3f292b7b13dc382e5aa287dde
Anduin Xue <anduin@aiursoft.com>
2024-09-24 10:35:51 +0000

    Rename UserId to OwnerId in Devices table
    Renamed the UserId column to OwnerId in the Devices table and updated associated foreign keys and indices. This change improves clarity by more accurately representing the relationship between devices and their owners.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20240924103536_RenameDeviceOwner.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDeviceOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_AspNetUsers_UserId",
+                table: "Devices");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Devices",
+                newName: "OwnerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Devices_UserId",
+                table: "Devices",
+                newName: "IX_Devices_OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_AspNetUsers_OwnerId",
+                table: "Devices",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_AspNetUsers_OwnerId",
+                table: "Devices");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerId",
+                table: "Devices",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Devices_OwnerId",
+                table: "Devices",
+                newName: "IX_Devices_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_AspNetUsers_UserId",
+                table: "Devices",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
