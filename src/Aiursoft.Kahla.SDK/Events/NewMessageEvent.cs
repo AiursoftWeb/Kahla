ab73fe93ccf70743944103fe49557e190e3ba6ba
Anduin Xue <anduin@aiursoft.com>
2024-09-24 12:09:04 +0000

    Add 'muted' parameter to NewMessageEvent constructor
    This commit introduces an optional 'muted' parameter to the NewMessageEvent constructor. It allows the initialization of the Muted property when creating a new instance. This change enhances the event's flexibility and usability.

--- src/Aiursoft.Kahla.SDK/Events/NewMessageEvent.cs
+++ src/Aiursoft.Kahla.SDK/Events/NewMessageEvent.cs
@@ -4,15 +4,14 @@ namespace Aiursoft.Kahla.SDK.Events;
 
 public class NewMessageEvent : KahlaEvent
 {
-    public NewMessageEvent(Message message)
+    public NewMessageEvent(Message message, bool muted = false)
     {
         Type = EventType.NewMessage;
         Message = message;
+        Muted = muted;
     }
-    
+
     public bool Muted { get; set; }
-        
     public int ConversationId => Message.ConversationId;
     public Message Message { get; set; }
-    public string? PreviousMessageId { get; set; }
 }
