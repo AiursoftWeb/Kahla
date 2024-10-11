203a0798b31cf3ab9f075b3deaeaa37db8da859d
Anduin Xue <anduin@aiursoft.com>
2024-10-11 13:35:31 +0000

    Add BlockRecords table in migrations
    Introduced a new table `BlockRecords` to manage user blocks, including fields for `CreatorId`, `TargetId`, `AddTime`, and `BlockTo`. This migration also updates the model snapshot and sets up necessary foreign keys and indices for optimizing database operations.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20241011133515_AddBlocks.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlocks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlockRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreatorId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TargetId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AddTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    BlockTo = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlockRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BlockRecords_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlockRecords_AspNetUsers_TargetId",
+                        column: x => x.TargetId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlockRecords_CreatorId",
+                table: "BlockRecords",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlockRecords_TargetId",
+                table: "BlockRecords",
+                column: "TargetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlockRecords");
+        }
+    }
+}
