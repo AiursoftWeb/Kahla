6424da1b602388ca502de8223db243b93d18ca10
Anduin Xue <anduin@aiursoft.com>
2024-10-12 07:36:15 +0000

    Rename UserThreadRelations column from 'Role' to 'UserThreadRole'.
    This migration changes the 'Role' column to 'UserThreadRole' in the UserThreadRelations table to enhance column naming clarity. Corresponding updates were made to the migration and model snapshot files.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20241012073600_AddRenameRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRenameRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "UserThreadRelations",
+                newName: "UserThreadRole");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserThreadRole",
+                table: "UserThreadRelations",
+                newName: "Role");
+        }
+    }
+}
