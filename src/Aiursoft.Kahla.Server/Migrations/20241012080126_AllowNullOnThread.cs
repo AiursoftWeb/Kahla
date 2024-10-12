ea7ccb40468fa4fcea5c4c7932b7f06bb4da888c
Anduin Xue <anduin@aiursoft.com>
2024-10-12 08:13:02 +0000

    Enable nullable OwnerRelationId in ChatThreads
    This change modifies the OwnerRelationId column in the ChatThreads table to allow null values. Foreign key constraints and relevant migrations are updated accordingly to support this new schema. This adjustment enhances the flexibility of the data model for scenarios where an owner relation may not be applicable.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20241012080126_AllowNullOnThread.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullOnThread : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatThreads_UserThreadRelations_OwnerRelationId",
+                table: "ChatThreads");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerRelationId",
+                table: "ChatThreads",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatThreads_UserThreadRelations_OwnerRelationId",
+                table: "ChatThreads",
+                column: "OwnerRelationId",
+                principalTable: "UserThreadRelations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatThreads_UserThreadRelations_OwnerRelationId",
+                table: "ChatThreads");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerRelationId",
+                table: "ChatThreads",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatThreads_UserThreadRelations_OwnerRelationId",
+                table: "ChatThreads",
+                column: "OwnerRelationId",
+                principalTable: "UserThreadRelations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
