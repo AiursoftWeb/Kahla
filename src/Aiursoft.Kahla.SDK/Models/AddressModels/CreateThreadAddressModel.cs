8cc236814a5209801c027a51ef57416233b3dfe8
Anduin Xue <anduin@aiursoft.com>
2024-10-13 17:20:08 +0000

    Add validation attributes to CreateThreadAddressModel
    Included `Required` and `StringLength` attributes for properties in `CreateThreadAddressModel` to enforce field validation. This will ensure all necessary data is correctly provided and within acceptable length limits.

--- src/Aiursoft.Kahla.SDK/Models/AddressModels/CreateThreadAddressModel.cs
+++ src/Aiursoft.Kahla.SDK/Models/AddressModels/CreateThreadAddressModel.cs
@@ -1,16 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aiursoft.Kahla.SDK.Models.AddressModels;
 
 public class CreateThreadAddressModel
 {
+    [Required]
+    [StringLength(256, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
     public required string Name { get; init; }
-    
+    [Required]
     public required bool AllowSearchByName { get; init; }
-    
+    [Required]
     public required bool AllowDirectJoinWithoutInvitation { get; init; }
-    
+    [Required]
     public required bool AllowMemberSoftInvitation { get; init; }
-    
+    [Required]
     public required bool AllowMembersSendMessages { get; init; } = true;
-    
+    [Required]
     public required bool AllowMembersEnlistAllMembers { get; init; } = true;
 }
