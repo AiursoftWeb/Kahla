09254155d505383b4db6b259c091c66ff20b0afb
Anduin Xue <anduin@aiursoft.com>
2024-10-13 12:37:44 +0000

    Remove 'Read' column from Messages table.
    This commit drops the 'Read' column from the Messages table and updates the migration snapshots accordingly. This change ensures the database schema is aligned with the current application model.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20241013123732_MessageNoLongerHasRead.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageNoLongerHasRead : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Read",
+                table: "Messages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Read",
+                table: "Messages",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
