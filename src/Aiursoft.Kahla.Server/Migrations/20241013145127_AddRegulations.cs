f812cb29d9e64c7ee92073e54e05e4c79e124bec
Anduin Xue <anduin@aiursoft.com>
2024-10-13 14:54:08 +0000

    Add regulation migration
    Introduce a new migration to alter the Reports table. The Reason field type has been changed from longtext to varchar(400) with a maximum length of 400 characters. This adjustment is mirrored in the migration's Down method for rollback purposes.

---
+++ src/Aiursoft.Kahla.Server/Migrations/20241013145127_AddRegulations.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aiursoft.Kahla.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegulations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Reports",
+                type: "varchar(400)",
+                maxLength: 400,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Reports",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)",
+                oldMaxLength: 400,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
