70c35f47277e10f283a064b85d7f692b133ad759
Anduin Xue <anduin@aiursoft.com>
2024-10-18 14:30:03 +0000

    Add SearchThreadsAsync test for verifying thread search functionality
    This commit introduces a new test method, `SearchThreadsAsync`, in `SearchTests.cs` which verifies the ability to search for threads globally. The test covers the creation of a private thread, attempts to search for it, and then makes the thread public to ensure it can be found through a global search.

--- tests/Kahla.Tests/SdkTests/SearchTests.cs
+++ tests/Kahla.Tests/SdkTests/SearchTests.cs
@@ -46,4 +46,32 @@ public class SearchTests : KahlaTestBase
         Assert.AreEqual(1, search21ByEmail.Users.Count);
         Assert.AreEqual(user21Id, search21ByEmail.Users.First().User.Id);
     }
+
+    [TestMethod]
+    public async Task SearchThreadsAsync()
+    {
+        // Prepare user1
+        await Sdk.RegisterAsync("user1@domain.com", "password");
+        var user1Id = (await Sdk.MeAsync()).User.Id;
+        await Sdk.SignoutAsync();
+
+        // Prepare user2
+        await Sdk.RegisterAsync("user2@domain.com", "password");
+
+        // Create a thread
+        var createdThread = await Sdk.HardInviteAsync(user1Id);
+
+        // Set the name.
+        await Sdk.UpdateThreadAsync(createdThread.NewThreadId, name: "Patched_name");
+
+        // Search the thread (Not found, because by default, the thread is private)
+        var searchThread = await Sdk.SearchThreadsGloballyAsync("Patched_name");
+        Assert.AreEqual(0, searchThread.Threads.Count);
+
+        // Make the thread public
+        await Sdk.UpdateThreadAsync(createdThread.NewThreadId, allowSearchByName: true);
+        var searchThread2 = await Sdk.SearchThreadsGloballyAsync("Patched_name");
+        Assert.AreEqual(1, searchThread2.Threads.Count);
+        Assert.AreEqual(createdThread.NewThreadId, searchThread2.Threads.First().Id);
+    }
 }
