ab1cae8f937e69ddbc2f516a9a346684628f687e
Anduin Xue <anduin@aiursoft.com>
2024-11-13 18:13:40 +0000

    Add ExcludeFromCodeCoverage attribute to relevant classes.
    Applied the ExcludeFromCodeCoverage attribute to the Program class and various migration classes/files to improve coverage reporting. This helps in excluding boilerplate or non-essential code from coverage metrics.
    
    Signed-off-by: Anduin Xue <anduin@aiursoft.com>

--- src/Aiursoft.Kahla.Server/Program.cs
+++ src/Aiursoft.Kahla.Server/Program.cs
@@ -1,13 +1,16 @@
-﻿using Aiursoft.DbTools;
+﻿using System.Diagnostics.CodeAnalysis;
+using Aiursoft.DbTools;
 using Aiursoft.Kahla.Server.Data;
+using Aiursoft.WebTools;
 
 namespace Aiursoft.Kahla.Server;
 
+[ExcludeFromCodeCoverage]
 public abstract class Program
 {
     public static async Task Main(string[] args)
     {
-        var app = await WebTools.Extends.AppAsync<Startup>(args);
+        var app = await Extends.AppAsync<Startup>(args);
         await app.UpdateDbAsync<KahlaRelationalDbContext>(UpdateMode.MigrateThenUse);
         await app.Services.GetRequiredService<QuickMessageAccess>().LoadAsync();
         await app.RunAsync();
