4e233716322a2a75827a16eb41f6c66698a8a262
Anduin Xue <anduin@aiursoft.com>
2025-10-28 06:52:45 +0000

    Patch gitlab.aiursoft.cn to gitlab.aiursoft.com.

--- src/Aiursoft.Kahla.Frontend/kahla.app/src/app/values.ts
+++ src/Aiursoft.Kahla.Frontend/kahla.app/src/app/values.ts
@@ -1,4 +1,4 @@
 export class Values {
     public static loadingImgURL = '/assets/192x192.png';
-    public static sourceCodeURL = 'https://gitlab.aiursoft.cn/aiursoft/kahla.app/-/commit/';
+    public static sourceCodeURL = 'https://gitlab.aiursoft.com/aiursoft/kahla.app/-/commit/';
 }
